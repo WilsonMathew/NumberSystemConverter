542c317a30d0800de526c422e7aa139fd37da614
WilsonMathew <wilsonmathewone@gmail.com>
2015-04-19 21:23:43 -0700

    fist commit

---
+++ NumberConverter.java
@@ -0,0 +1,21 @@
+public class NumberConverter {
+
+// suffix
+// Binary = b
+// Decimal = d
+// Octal = o
+// Hexadecimal = h
+
+	public static void main(String[] args) {
+		System.out.println(args[0]);
+	}
+
+	/**
+	* This method will convert any number into binary format
+	*
+	**/	
+	public static String ConverToBinary(String number) {
+			
+		return "binary number"
+	}
+}
