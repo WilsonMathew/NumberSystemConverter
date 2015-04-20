c1f1369768d99a2d23d98e48add22b7468a0ecf3
WilsonMathew <wilsonmathewone@gmail.com>
2015-04-19 22:19:13 -0700

    Added header of the first funtion ConvertBinary to Octal, decimal, and Hex.Use two args[0] and args[1]

--- NumberConverter.java
+++ NumberConverter.java
@@ -5,17 +5,32 @@ public class NumberConverter {
 // Decimal = d
 // Octal = o
 // Hexadecimal = h
-
+/**
+if I have a Binary, it need to be converted to Decimal, Octal and Hexadcimal.
+**/
 	public static void main(String[] args) {
-		System.out.println(args[0]);
+
+		if(args[0].equals("b")) {
+			System. out.println(args[1]  + " It's a binary number");
+		}	
 	}
 
 	/**
 	* This method will convert any number into binary format
 	*
 	**/	
-	public static String ConverToBinary(String number) {
-			
-		return "binary number"
+
+	public static String ConverBinary(String number) {
+
+		// determine what number it is
+
+		// if decimal then 
+		// do process to convert to binary
+		// else if ocatal
+		// do process to convert to binary
+		// else if hexadecimal 
+		// do process to convert to binary
+	
+		return "binary number";
 	}
 }
