f92b67e1f2888e089c53be9531e17be110d20ced
WilsonMathew <wilsonmathewone@gmail.com>
2015-04-26 19:52:08 -0700

    wrote som in the Binary_ToOctate method. Grouped binary in 3 and completed binary string with 0 when not multiple of 3

--- NumberConverter.java
+++ NumberConverter.java
@@ -6,19 +6,51 @@ public class NumberConverter {
 // Octal = o
 // Hexadecimal = h
 /**
-if I have a Binary, it need to be converted to Decimal, Octal and Hexadcimal.
+ * if I have a Binary, it need to be converted to Decimal, Octal and Hexadcimal.
+ *
+ * Let's start with the simplest operations which is to convert a binary to a Octal.
+ * 100101 octal. --->  45
+ *
 **/
 	public static void main(String[] args) {
-
-		if(args[0].equals("b")) {
-			System. out.println(args[1]  + " It's a binary number");
-		}	
+		System.out.println(B_ToOctal(args[0]));
+		System.out.println("abc".substring(1,2));
 	}
 
 	/**
 	* This method will convert any number into binary format
 	*
 	**/	
+	public static String B_ToOctal(String hello) {
+
+		int binary_length = hello.length();	
+
+		//  Make the binary a multiple of 3
+		while(binary_length%3!=0) {
+			hello= "0" + hello;	
+			binary_length = hello.length();
+		}
+
+		// Determine how many octates I have.
+		int n_octates = binary_length/3;
+
+		// substring starts at index 0 and ends at index-1;
+		int binary_begin=0;
+		int binary_end=3;
+		
+		int i = 1;
+
+		// split it into groups
+		while(i <= n_octates) {
+			String temp = hello.substring(binary_begin,binary_end);
+			binary_begin+=3;
+			binary_end+=3;
+			i++;
+			System.out.println(temp);
+		}
+
+		return hello;
+	} 
 
 	public static String ConverBinary(String number) {
 
